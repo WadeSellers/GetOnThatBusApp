d85a7522c348be3c9882bec405e6ad7030c7e6de
Wade Sellers <wade@wadesellers.com>
2023-06-23 16:29:48 -0400

    added local bus station annotations

--- GetOnThatBus/ViewController.swift
+++ GetOnThatBus/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let address = "https://api.wmata.com/Bus.svc/json/jStops/"
+    let apiKey = "?api_key=e13626d03d8e4c03ac07f95541b3091b"
+    let region = "&Lat=38.89731&Lon=-77.00626&Radius=2000"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         locationManager.delegate = self
+        
+        let url = URL(string: "\(address)\(apiKey)\(region)")!
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                
+                self.parse(json: json)
+            } else {
+                self.loadError()
+            }
+        }
         
     }
     
@@ -31,6 +46,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let region = MKCoordinateRegion(center: center, span: span)
         self.mapView.setRegion(region, animated: true)
         
+    }
+    
+    // Parsing the JSON
+    
+    func parse(json: JSON) {
+        for result in json["Stops"].arrayValue {
+            let latitude = result["Lat"].doubleValue
+            let longitude = result["Lon"].doubleValue
+            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func loadError() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the bus stop data", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok:", style: .default))
+            self.present(alert, animated: true)
+        }
     }
     
     
